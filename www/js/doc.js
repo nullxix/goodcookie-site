6c149546aaf47a7c570134519924ecb6053a2c7b
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-03-19 12:33:29 -0600

    testing colors

---
+++ www/js/doc.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById('doc-nav')
+const navItems = document.getElementById('doc-nav-items')
+const navToggleButton = document.getElementById('doc-nav-toggle-button')
+const toggleNav = event => {
+
+}

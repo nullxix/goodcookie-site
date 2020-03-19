8700b4f678e496dd80cfbbcdbcb10b5be02ff9a0
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-03-19 13:52:25 -0600

    basic transitions

--- www/js/doc.js
+++ www/js/doc.js
@@ -1,6 +1,19 @@
 const nav = document.getElementById('doc-nav')
 const navItems = document.getElementById('doc-nav-items')
 const navToggleButton = document.getElementById('doc-nav-toggle-button')
+let toggleOpen = true;
 const toggleNav = event => {
+    toggleOpen = !toggleOpen
 
+    if(toggleOpen){
+        navItems.classList.remove('toggle-hide')
+        nav.classList.remove('toggle-narrow')
+        navToggleButton.classList.remove('toggle-down')
+    } else {
+        navItems.classList.add('toggle-hide')
+        nav.classList.add('toggle-narrow')
+        navToggleButton.classList.add('toggle-down')
+    }
 }
+
+navToggleButton.addEventListener('click', toggleNav)
